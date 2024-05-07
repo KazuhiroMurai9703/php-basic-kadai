49f2a8824f713302e92753d939f0222b18008eef
かずひろ9703 <166223246+KazuhiroMurai9703@users.noreply.github.com>
2024-05-07 23:23:25 +0900

    PHPの基礎を学ぼう/ 連想配列を作って値とキーを出力しよう/課題をプッシュ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>SAMURAI課題/PHPの基礎を学ぼう/テストの平均点を計算しよう</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+
+        // 連想配列
+        $data = ['name' => 'onion', 'price' => 200, 'weight' => '男性'];
+
+        // 連想配列の値を出力する
+        print_r($data);
+
+        ?>
+     </p>
+ </body>
+ 
+ </html>
