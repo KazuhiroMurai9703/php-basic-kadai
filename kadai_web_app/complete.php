ec14aca9410dd86b64f64c60b70e8221f832f72b
かずひろ9703 <166223246+KazuhiroMurai9703@users.noreply.github.com>
2024-05-17 06:15:10 +0900

    PHPの基礎を学ぼう/ 社員情報入力フォームを作成しよう / 課題をプッシュ

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>社員情報入力フォームを作成しよう</title>
+</head>
+
+<body>
+    <h2>お問い合わせを承りました。</h2>
+    <p>ありがとうございました。今後の参考にさせていただきます。</p>
+    <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</body>
+
+</html>
